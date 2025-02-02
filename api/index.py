ed49516c536722c7eebfdf21699f2a03264cc51e
Arun P <21f1001681@ds.study.iitm.ac.in>
2025-02-02 17:42:36 +0530

    ft: modified code

--- api/index.py
+++ api/index.py
@@ -24,10 +24,11 @@ with open(data_file, 'r') as f:
 async def get_marks(names: Optional[List[str]] = Query(None)):
     if names is None:
         raise HTTPException(status_code=400, detail="Names must be provided")
-    marks_list = [
-        next((student["marks"] for student in marks_full_list if student["name"] == name), "Not Found")
-        for name in names
-    ]
+    
+    marks_list = []
+    for name in names:
+        mark = next((student["marks"] for student in marks_full_list if student["name"] == name), "Not Found")
+        marks_list.append(mark)
     
     return {"marks" : marks_list}
 
